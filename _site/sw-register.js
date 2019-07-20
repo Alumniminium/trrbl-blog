0c4aa2ddd6dc8aa6238d01a8772aca959466b17e
alumniminium <domi@outlook.at>
2019-07-20 20:19:03 +0200

    onFileChangeHook

--- _site/sw-register.js
+++ _site/sw-register.js
@@ -1,5 +1,5 @@
         if ('serviceWorker' in navigator) {
-            navigator.serviceWorker.register('/service-worker.js?v=1563646706').then(function(reg) {
+            navigator.serviceWorker.register('/service-worker.js?v=1563646743').then(function(reg) {
                 reg.onupdatefound = function() {
                     var installingWorker = reg.installing;
                     installingWorker.onstatechange = function() {
