64a8111dbb95b68b46af47c3d2639dd3abe6cf26
trrbl <domi@outlook.at>
2019-11-23 21:05:11 +0100

    onFileChangeHook

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,20 @@
 workbox.core.setCacheNameDetails({
     prefix: 'trrbl',
-    suffix: 'v1',
+    suffix: 'v2',
     precache: 'precache',
     runtime: 'runtime-cache'
 });
 workbox.skipWaiting();
 workbox.clientsClaim();
 workbox.precaching.precacheAndRoute(self.__precacheManifest);
-workbox.routing.registerRoute(/\.html$/, workbox.strategies.precacheAndRoute());
-workbox.routing.registerRoute(/\.ttf$/, workbox.strategies.precacheAndRoute());
-workbox.routing.registerRoute(/\.woff$/, workbox.strategies.precacheAndRoute());
-workbox.routing.registerRoute(/\.js$/, workbox.strategies.precacheAndRoute());
-workbox.routing.registerRoute(/\.css$/, workbox.strategies.precacheAndRoute());
-workbox.routing.registerRoute(/\.webp$/, workbox.strategies.precacheAndRoute());
-workbox.routing.registerRoute(/\.png$/, workbox.strategies.precacheAndRoute());
-workbox.routing.registerRoute(/assets\/(img|icons)/, workbox.strategies.precacheAndRoute());
-workbox.routing.registerRoute(/^https?:\/\/h.img.alumni.re/, workbox.strategies.staleWhileRevalidate());
+workbox.routing.registerRoute(/\.html$/, workbox.strategies.StaleWhileRevalidate());
+workbox.routing.registerRoute(/\.js$/, workbox.strategies.StaleWhileRevalidate());
+workbox.routing.registerRoute(/\.css$/, workbox.strategies.StaleWhileRevalidate());
+workbox.routing.registerRoute(/\.webp$/, workbox.strategies.StaleWhileRevalidate());
+workbox.routing.registerRoute(/\.png$/, workbox.strategies.StaleWhileRevalidate());
+workbox.routing.registerRoute(/\.jpg$/, workbox.strategies.StaleWhileRevalidate());
+workbox.routing.registerRoute(/assets\/(img|icons)/, workbox.strategies.StaleWhileRevalidate());
+workbox.routing.registerRoute(/^https?:\/\/h.img.alumni.re/, workbox.strategies.StaleWhileRevalidate());
 self.addEventListener('fetch', function (event) {
     event.respondWith(caches.match(event.request).then(function (response) {
         return response || fetch(event.request)
